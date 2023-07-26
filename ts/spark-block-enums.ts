36cc62f4caa5e5dc9d977453d21908e450d870bd
Bob Steers <steers.bob@gmail.com>
2023-07-26 10:36:27 +0200

    Rename GPIO LOAD_DETECT enum values

--- ts/spark-block-enums.ts
+++ ts/spark-block-enums.ts
@@ -113,16 +113,15 @@ export const GpioDeviceType = Enum(
   'GPIO_DEV_MOTOR_2P_BIDIRECTIONAL',
   'GPIO_DEV_MOTOR_1P_HIGH_SIDE',
   'GPIO_DEV_MOTOR_1P_LOW_SIDE',
-  'GPIO_DEV_GND_POWER_2P_LOAD_DETECT_LOW_CURRENT',
-  'GPIO_DEV_GND_1P_LOAD_DETECT_LOW_CURRENT',
-  'GPIO_DEV_POWER_1P_LOAD_DETECT_LOW_CURRENT',
+  'GPIO_DEV_DETECT_LOW_CURRENT_2P',
+  'GPIO_DEV_DETECT_LOW_CURRENT_1P_GND',
+  'GPIO_DEV_DETECT_LOW_CURRENT_1P_POWER',
+  'GPIO_DEV_DETECT_HIGH_CURRENT_1P_POWER',
+  'GPIO_DEV_DETECT_HIGH_CURRENT_1P_GND',
+  'GPIO_DEV_DETECT_HIGH_CURRENT_2P',
   'GPIO_DEV_POWER_1P',
-  'GPIO_DEV_POWER_1P_LOAD_DETECT_HIGH_CURRENT',
   'GPIO_DEV_GND_1P',
-  'GPIO_DEV_GND_1P_LOAD_DETECT_HIGH_CURRENT',
-  'GPIO_DEV_GND_POWER_2P_LOAD_DETECT_HIGH_CURRENT',
 );
-
 
 export enum GpioPins {
   NONE = 0,
@@ -338,13 +337,9 @@ export const WifiCipherType = Enum(
 
 // #region DigitalInput
 
-export const ToggleBehavior = Enum(
-  'DIRECT',
-  'ALTERNATING',
-);
+export const ToggleBehavior = Enum('DIRECT', 'ALTERNATING');
 
 // #endregion DigitalInput
-
 
 export type BlockIntfType = Enum<typeof BlockIntfType>;
 export type SystemBlockType = Enum<typeof SystemBlockType>;
